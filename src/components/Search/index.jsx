ca9ee1def68a84770b7faab77d445ac69187df81
Yuriy Ulanov <1859697@gmail.com>
2023-02-04 18:11:58 +0300

    #10 pagination & search

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import styles from './Search.module.scss';
+
+const Search = ({ searchValue, setSearchValue }) => {
+  return (
+    <div className={styles.root}>
+      <svg className={styles.icon} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z" />
+      </svg>
+      <input
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        className={styles.input}
+        placeholder="Поиск пиццы..."
+      />
+      {searchValue && (
+        <svg
+          onClick={() => setSearchValue('')}
+          className={styles.close}
+          id="Layer_1"
+          version="1.1"
+          viewBox="0 0 512 512"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z" />
+        </svg>
+      )}
+    </div>
+  );
+};
+
+export default Search;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,13 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Sceleton from '../components/PizzaBlock/Sceleton';
+import Pagination from '../components/Pagination';
 
-const Home = () => {
-  const [pizzas, setPizzas] = useState([]);
+const Home = ({ searchValue }) => {
+  const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [sortType, setSortType] = useState({
     name: 'популярности',
     sortProperty: 'rating',
@@ -17,20 +19,27 @@ const Home = () => {
 
   useEffect(() => {
     setIsLoading(true);
+
+    const search = searchValue ? `&search=${searchValue}` : '';
+
     fetch(
-      `https://63cc411eea855154151e1425.mockapi.io/items?${
+      `https://63cc411eea855154151e1425.mockapi.io/items?page=${currentPage}&limit=4&${
         categoryId > 0 ? `category=${categoryId}` : ''
-      }&sortBy=${sortType.sortProperty}&order=${order}`,
+      }&sortBy=${sortType.sortProperty}&order=${order}${search}`,
     )
       .then((res) => {
         return res.json();
       })
       .then((json) => {
-        setPizzas(json);
+        setItems(json);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, order]);
+  }, [categoryId, sortType, searchValue, currentPage, order]);
+
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+
+  const sceletons = [...new Array(6)].map((_, index) => <Sceleton key={index} />);
 
   const onOrderChange = () => {
     order === 'desc' ? setOrder('ask') : setOrder('desc');
@@ -49,11 +58,8 @@ const Home = () => {
         />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Sceleton key={index} />)
-          : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? sceletons : pizzas}</div>
+      <Pagination onChangePage={(nubmer) => setCurrentPage(nubmer)} />
     </div>
   );
 };
